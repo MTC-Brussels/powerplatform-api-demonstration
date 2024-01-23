8709c90fcb29b0eec7c8e9c54eb173696f966bad
Dries Augustyns <daugustyns@microsoft.com>
2024-01-23 07:21:55 +0000

    Delete obsolete test files

--- tests/http.js
+++ tests/http.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const { app, server } = require('../index');
 
-describe('HTTP GET Endpoints', () => {
+describe('HTTP Endpoints', () => {
   afterAll((done) => {
     server.close();
     done();
@@ -24,6 +24,19 @@ describe('HTTP GET Endpoints', () => {
     expect(res.statusCode).toEqual(200);
 
     // The response should be an object
+    expect(res.body).toHaveProperty('id');
+  });
+
+  it('should create a new employee', async () => {
+    const res = await request(app).post('/employees').send({
+      name: 'Dries Augustyns',
+      position: 'Technical Architect',
+    });
+
+    // The result should return a 200 status code
+    expect(res.statusCode).toEqual(200);
+
+    // If the employee was created successfully, the response should contain the new employee's ID
     expect(res.body).toHaveProperty('id');
   });
 });
