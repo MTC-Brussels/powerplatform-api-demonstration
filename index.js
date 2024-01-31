0bcb2fa6dd6e99733706742797a36fc52d85fe60
Dries Augustyns <daugustyns@microsoft.com>
2024-01-31 07:56:21 +0000

    Refactor employee endpoints

--- index.js
+++ index.js
@@ -50,39 +50,6 @@ app.get('/employees', (req, res) => {
   res.json(employees);
 });
 
-// PUT route to update an employee
-app.put('/employees/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const employee = employees.find((emp) => emp.id === id);
-
-  if (!employee) {
-    return res.status(404).json({ error: 'Employee not found' });
-  }
-
-  if (req.body.name) {
-    employee.name = req.body.name;
-  }
-
-  if (req.body.position) {
-    employee.position = req.body.position;
-  }
-
-  res.json(employee);
-});
-
-// DELETE route to delete an employee
-app.delete('/employees/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = employees.findIndex((emp) => emp.id === id);
-
-  if (index === -1) {
-    return res.status(404).json({ error: 'Employee not found' });
-  }
-
-  employees.splice(index, 1);
-  res.sendStatus(204);
-});
-
 // Start the server
 const server = app.listen(8080, () => {
   console.log('Server is running on port 8080');
