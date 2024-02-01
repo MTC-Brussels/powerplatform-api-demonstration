d4bcb791f60dda04e8372a77ca69c00a0040f7bd
Dries Augustyns <daugustyns@microsoft.com>
2024-02-01 07:09:43 +0000

    Update API endpoints and remove unnecessary code

--- index.js
+++ index.js
@@ -50,41 +50,6 @@ app.get('/employees', (req, res) => {
   res.json(employees);
 });
 
-// PUT route to update an employee
-app.put('/employees/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const employee = employees.find((emp) => emp.id === id);
-
-  if (!employee) {
-    return res.status(404).json({ error: 'Employee not found' });
-  }
-
-  if (req.body.name) {
-    employee.name = req.body.name;
-  }
-
-  if (req.body.position) {
-    employee.position = req.body.position;
-  }
-
-  res.json(employee);
-});
-
-// DELETE route to delete an employee
-app.delete('/employees/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const employee = employees.find((emp) => emp.id === id);
-
-  if (!employee) {
-    return res.status(404).json({ error: 'Employee not found' });
-  }
-
-  const index = employees.indexOf(employee);
-  employees.splice(index, 1);
-
-  res.json(employee);
-});
-
 // Start the server
 const server = app.listen(8080, () => {
   console.log('Server is running on port 8080');
