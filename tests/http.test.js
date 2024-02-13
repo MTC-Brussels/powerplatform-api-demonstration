053a8ee499a18dffe4646f7281ebb6fb16f318b5
Dries Augustyns <daugustyns@microsoft.com>
2024-02-13 06:43:47 +0000

    Removed PUT and DELETE routes

--- tests/http.test.js
+++ tests/http.test.js
@@ -39,27 +39,4 @@ describe('HTTP Endpoints', () => {
     // If the employee was created successfully, the response should contain the new employee's ID
     expect(res.body).toHaveProperty('id');
   });
-
-  it('should update an employee', async () => {
-    const res = await request(app).put('/employees/1').send({
-      name: 'Dries Augustyns',
-      position: 'Technical Architect',
-    });
-
-    // The result should return a 200 status code
-    expect(res.statusCode).toEqual(200);
-
-    // The response should be an object
-    expect(res.body).toHaveProperty('id');
-  });
-
-  it('should delete an employee', async () => {
-    const res = await request(app).delete('/employees/1');
-
-    // The result should return a 200 status code
-    expect(res.statusCode).toEqual(200);
-
-    // The response should be an object
-    expect(res.body).toHaveProperty('id');
-  });
 });
